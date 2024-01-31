9488be757b22b00593108b771b46ab501a049094
page0715 <page0715@gmail.com>
2024-01-31 15:29:23 +0800

    建立新增商品表單頁面

--- back/th.php
+++ back/th.php
@@ -81,7 +81,9 @@ function addType(type){
 
 
 <h2 class="ct">商品管理</h2>
-<div class="ct"><button>新增商品</button></div>
+<div class="ct">git add <div class=""></div>
+    <button onclick="location.href='?do=add_goods'">新增商品</button>
+</div>
 <table class="all">
     <tr class="tt ct">
         <td>編號</td>
