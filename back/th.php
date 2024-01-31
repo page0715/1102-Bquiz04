190e401cba7071d2d9b0766bfecfe6b196738206
page0715 <page0715@gmail.com>
2024-01-31 15:21:08 +0800

    使用ajax 來完成修改分類功能

--- back/th.php
+++ back/th.php
@@ -42,6 +42,16 @@ foreach($bigs as $big){
 <script>
 getTypes(0)
 
+function edit(dom,id){
+    let name=prompt("請輸入你要修改的分類名稱:",`${$(dom).parent().prev().text()}`)
+    if(name!=null){
+        $.post("./api/save_type.php",{name,id},()=>{
+            $(dom).parent().prev().text(name)
+            //location.reload();
+        })
+    }
+}
+
 function getTypes(big_id){
     $.get("./api/get_types.php",{big_id},(types)=>{
         $("#bigs").html(types)
